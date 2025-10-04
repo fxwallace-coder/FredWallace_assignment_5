e7c873c58bd72be2b0b0938fa0f7e19bd22c61d7
fxwallace-coder <fxwallace@aggies.ncat.edu>
2025-10-03 21:04:28 -0400

    Update FredWallace_assignment_5.py
    "Challenge 2: Prime checker - for loop for known range"

--- FredWallace_assignment_5.py
+++ FredWallace_assignment_5.py
@@ -1,3 +1,4 @@
+#1st Challenge
 current_number = int(input("Enter starting number: "))
 step_count = 0
 print(f"Starting number: {current_number}")
@@ -12,3 +13,15 @@ while current_number !=1:
         print(f"{current_number:.0f}",end=" ")
 print()
 print(f"steps: {step_count}")
+
+#2nd Challenge
+print("=== Challenge 2: Prime Number Checker ===")
+num_input=int(input("Enter a number:"))
+print(f" Testing divisors from 2 to {num_input-1}...")
+for i in range(2,num_input-1):
+    if num_input%i==0:
+        print(f"{num_input} is not prime (divisible by {i})")
+        break
+else:
+    print(f"{num_input} is prime!")    
+print()
